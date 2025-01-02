280050f75e280fb18be506e4cd833fa43872cbcd
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-01-02 09:35:08 +0700

    add field, method, constructor

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,14 @@
 class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
 
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
+    void sayHello(String paramName){
+        System.out.println("Hello " + paramName + " My name is " + name );
+    }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,12 +1,26 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person();
-        Person person2 = new Person();
-        Person person3;
-        person3 = new Person();
+        var person1 = new Person("Denar", "Cirebon");
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+        person1.sayHello("Budi");
+
+        Person person2 = new Person("Tobi", "Konoha");
+
+        System.out.println(person2.name);
+        System.out.println(person2.address);
+        System.out.println(person2.country);
+        person2.sayHello("Mr. Robot");
+
+        Person person3;
+        person3 = new Person("Ciro", "Bandung");
+
+        System.out.println(person3.name);
+        System.out.println(person3.address);
+        System.out.println(person3.country);
+        person3.sayHello("David");
+
     }
 }
