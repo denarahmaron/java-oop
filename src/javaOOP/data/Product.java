d046ec9cafda5b09943606d05c047e8c7a0f7c2e
denarahmaron <denarahmaron1@gmail.com>
2025-01-04 14:44:28 +0700

    add package

---
+++ src/javaOOP/data/Product.java
@@ -0,0 +1,11 @@
+package javaOOP.data;
+
+class Product {
+    String productName;
+    int price;
+
+    Product(String productName, int price){
+        this.productName = productName;
+        this.price = price;
+    }
+}
