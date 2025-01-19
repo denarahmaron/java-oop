072e815ed1e2fb945f26ecbea3503074afc4b8e6
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-01-19 23:04:16 +0700

    add exception

---
+++ src/javaOOP/application/ValidationApp.java
@@ -0,0 +1,20 @@
+package javaOOP.application;
+
+import javaOOP.data.LoginRequest;
+import javaOOP.error.ValidationException;
+import javaOOP.util.ValidationUtil;
+
+public class ValidationApp {
+    public static void main(String[] args) {
+        LoginRequest loginRequest = new LoginRequest("Ciro", null);
+
+        try{
+            ValidationUtil.validate(loginRequest);
+            System.out.println("Data Valid");
+        } catch (ValidationException | NullPointerException e){
+            System.out.println("Terjadi error dengan pesan : " + e.getMessage());
+        } finally {
+            System.out.println("Selesai");
+        }
+    }
+}
---
+++ src/javaOOP/error/ValidationException.java
@@ -0,0 +1,7 @@
+package javaOOP.error;
+
+public class ValidationException extends Throwable {
+    public ValidationException(String message){
+        super(message);
+    }
+}
